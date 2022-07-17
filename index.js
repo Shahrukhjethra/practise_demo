84dfeafe16cf4000a5f273b7f0b8aee111fac266
“Shahrukh” <shahrukh.jethra@appitventures@com>
2022-07-17 12:21:24 +0530

    Dsd

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const controller = require('./controller')
+
+app.post('/', () => {
+    controller.index();
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
